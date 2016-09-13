1400525c2860b55a12d152eefd26455622f6d38d
jd-erreape <juandediosherrero@gmail.com>
2016-09-13 22:47:00 +0200

    application-logic - Add vote function to core logic

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -2,7 +2,7 @@ import {List, Map} from 'immutable';
 
 export function setEntries (state, entries) {
   return state.set('entries', List(entries));
-}
+};
 
 export function next (state) {
   const entries = state.get('entries');
@@ -11,4 +11,12 @@ export function next (state) {
     vote: Map({pair: entries.take(2)}),
     entries: entries.skip(2)
   });
+};
+
+export function vote(state, entry) {
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+  );
 };
