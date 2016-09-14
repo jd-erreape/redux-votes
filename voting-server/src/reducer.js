4aaea293f3f0d843364dea63364dd8d2e45e9eba
jd-erreape <juandediosherrero@gmail.com>
2016-09-14 22:12:38 +0200

    add-reducer - Basic reducer composition

--- voting-server/src/reducer.js
+++ voting-server/src/reducer.js
@@ -1,16 +1,13 @@
 import {setEntries, next, vote, INITIAL_STATE} from './core';
 
 export default function reducer  (state=INITIAL_STATE, action) {
-  console.log('applying action: ' + action.type);
-  console.log(state);
-
   switch (action.type) {
     case 'SET_ENTRIES':
       return setEntries(state, action.entries);
     case 'NEXT':
       return next(state);
     case 'VOTE':
-      return vote(state, action.entry);
+      return state.update('vote', voteState => vote(voteState, action.entry));
     default:
       return state;
   }
